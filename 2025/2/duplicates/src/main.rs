5692335529483bea77b665ed42eed558cfb27c8c
Jacob Deinum <jdeinum@nullspaces.org>
2025-12-02 11:10:09 -0700

    checkpointing: part 1 done, onto dp for part 2

--- 2025/2/duplicates/src/main.rs
+++ 2025/2/duplicates/src/main.rs
@@ -1,25 +1,47 @@
-// Something I unsure about is whether the numbers can be represented as numbers, or if I need to
-// make them strings, depends entirely if they are smaller than u64::max
+use anyhow::anyhow;
+use std::io::Read;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 fn main() -> Result<()> {
-    todo!()
+    let path = std::env::args()
+        .into_iter()
+        .skip(1)
+        .next()
+        .ok_or_else(|| anyhow!("No path provided"))?;
+
+    // open file
+    let mut f = std::fs::File::open(&path).context("open path to file")?;
+
+    // read all of the lines
+    let mut buf: String = String::new();
+    f.read_to_string(&mut buf)
+        .context("read bytes into string")?;
+
+    let res = part1(&buf).context("run part 1")?;
+    println!("res: {res}");
+
+    Ok(())
 }
 
-fn part1(ranges: &str) -> Result<Vec<String>> {
-    todo!()
+fn part1(ranges: &str) -> Result<u64> {
+    let id_ranges = parse_id_ranges(ranges).context("parse id ranges")?;
+    let res = id_ranges.iter().map(|x| x.find_invalid()).flatten().sum();
+    Ok(res)
 }
 
 fn parse_id_ranges(ranges: &str) -> Result<Vec<IdRange>> {
-    let raw_ranges: Result<Vec<IdRange>> =
-        ranges.split(",").map(|x| IdRange::try_from(x)).collect();
+    let raw_ranges: Result<Vec<IdRange>> = ranges
+        .trim()
+        .split(",")
+        .map(|x| IdRange::try_from(x))
+        .collect();
     raw_ranges
 }
 
 struct IdRange {
-    starting_number: String,
-    ending_number: String,
+    starting_number: u64,
+    ending_number: u64,
 }
 
 impl TryFrom<&str> for IdRange {
@@ -31,15 +53,42 @@ impl TryFrom<&str> for IdRange {
             .ok_or_else(|| anyhow::Error::msg("Range must contain -"))?;
 
         Ok(Self {
-            starting_number: starting_number.to_string(),
-            ending_number: ending_number.to_string(),
+            starting_number: starting_number
+                .parse()
+                .with_context(|| format!("parse starting number: {starting_number}"))?,
+            ending_number: ending_number
+                .parse()
+                .with_context(|| format!("parse ending_number number: {ending_number}"))?,
         })
     }
 }
 
 impl IdRange {
-    pub fn find_invalid(&self) -> bool {
-        todo!()
+    pub fn find_invalid(&self) -> Vec<u64> {
+        let mut res: Vec<u64> = Vec::new();
+
+        // need end to be included
+        for x in self.starting_number..self.ending_number + 1 {
+            // obvious way is to convert to a string, but there is probably a better way
+            // another small optimization here is to use a different allocator that handles similar
+            // sized allocations well like slub
+            let s = x.to_string();
+            let len = s.len();
+
+            // any odd number of characters can't be split into 2 of the same numbers
+            if len % 2 == 1 {
+                continue;
+            }
+
+            let (l, r) = s.split_at(len / 2);
+
+            if l == r {
+                println!("{l} == {r} so adding to set");
+                res.push(x);
+            }
+        }
+
+        res
     }
 }
 
@@ -47,4 +96,16 @@ impl IdRange {
 mod tests {
 
     use super::*;
+
+    #[test]
+    fn test_part1_example() -> Result<()> {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,\
+            446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+        let res = part1(&input).context("run part 1")?;
+
+        assert_eq!(res, 1227775554);
+
+        Ok(())
+    }
 }
